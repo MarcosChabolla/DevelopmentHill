df6f2e2739d61e2401ea4fe8e51a817c56f5e0a2
Marcos <root@Marcoss-MacBook-Pro.local>
2018-05-15 17:02:58 -0700

    Development hill underconstruction. Authentication, Login, Registration, Routing, and Databasing Done. Processing...

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="card">
+	<div class="card-header">Create new Post</div>
+
+	<div class="card-body">
+		<form action="{{ route('post.store') }}" method="post"> 
+			{{csrf_field()}}
+			<div class="form-group">
+				<label for="title">Title</label>
+				<input type = "text" name = "title" class="form-control"></input>
+			</div>
+			<div class="form-group">
+				<label for="featured">Featured Image</label>
+				<input type = "file" name = "featured" class="form-control"></input>
+			</div>
+			<div class="form-group">
+				<label for="content">Content</label>
+				<textarea name="content" id="content" cols="5" rows = "5" class="form-control"></textarea>
+			</div>
+			<div class="form-group">
+				<div class="text-center">
+					<button class="btn btn-success" type="submit">Store Post</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+	
+@stop
